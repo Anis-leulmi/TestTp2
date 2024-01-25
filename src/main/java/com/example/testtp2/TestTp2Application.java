d6d029041a7aead0931b8ab60848bc95094c14cb
Anis-leulmi <leulmi_anis@hotmail.com>
2024-01-24 22:36:00 -0500

    Initial commit

---
+++ src/main/java/com/example/testtp2/TestTp2Application.java
@@ -0,0 +1,13 @@
+package com.example.testtp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testtp2/TestTp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
