c8e6f70560632193a0425f25c9b2845ca4e3a7f4
Anis-leulmi <leulmi_anis@hotmail.com>
2024-01-26 20:49:34 -0500

    Ajout de l'annotation @RestController pour définir HomeController comme une classe contrôleur d'API REST

--- src/main/java/com/example/testtp2/controller/HomeController.java
+++ src/main/java/com/example/testtp2/controller/HomeController.java
@@ -2,5 +2,7 @@ package com.example.testtp2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 }
