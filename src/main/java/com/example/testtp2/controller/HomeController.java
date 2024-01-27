e1ae83ed58fb2d331c7f800dab82025fe8210cb5
Anis-leulmi <leulmi_anis@hotmail.com>
2024-01-26 20:47:04 -0500

    Importations des annotations Spring MVC.

--- src/main/java/com/example/testtp2/controller/HomeController.java
+++ src/main/java/com/example/testtp2/controller/HomeController.java
@@ -1,4 +1,6 @@
 package com.example.testtp2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 }
